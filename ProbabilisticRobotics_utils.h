d2e7c8df78987b7573842f992404653c473b8bf1
francesco <francesco.chichi@gmail.com>
2017-05-16 20:13:12 +0200

    1.0

--- ProbabilisticRobotics_utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-namespace pr {
-  //! returns the current time in milliseconds
-  double getTime(); 
-
-  //! prints a null terminated array od strings
-  void printBanner(const char** banner, std::ostream& os=std::cout);
-}
